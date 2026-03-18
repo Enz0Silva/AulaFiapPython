979ecf7491be60cc6cbab4ab5903b858ce811397
Enz0Silva <labsfiap@fiap.com.br>
2026-03-18 20:47:19 -0300

    Aula 2 variaveis

---
+++ Aula02 - variaveis - operadores/Aula2a-variaveis.py
@@ -0,0 +1,37 @@
+print("Olá Mundo")
+
+print(7+4)
+print("7+4")
+print("7+6")
+
+# Variáveis
+nome = "Enzo"
+print(f"Meu nome é: {nome}")
+idade = 18
+altura = 1.70
+
+# Input
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: "))
+peso = float(input("Digite o seu peso: "))
+
+print(nome, idade, peso)
+
+anoNascimento = 2007
+anoAtual = 2026
+idade = anoAtual - anoNascimento
+print(idade)
+
+
+nome1 ="Boas Vindas Enzo"
+print(nome1)
+dia = 3
+mes = 18
+ano = 2026
+print(f"Nós estamos no: {dia}/{mes}/{ano}")
+
+number = 10
+number2 = 6
+soma = number + number2
+print(f"soma")
+
