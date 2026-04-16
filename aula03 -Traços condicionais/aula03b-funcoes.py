bd49887ac4f778e003def5e211a78791328fe05f
main <r2enzo@gmail.com>
2026-04-15 21:56:41 -0300

    Aula de Fuções

--- aula03 -Traços condicionais/aula03b-funcoes.py
+++ aula03 -Traços condicionais/aula03b-funcoes.py
@@ -1,5 +1,27 @@
-def dobro(numero):
-    print(numero*2)
+def boas_vindas(nome):
+    print(f"Olá {nome}!! Seja bem-vindo!!")
 
-dobro(3)
+nome_digitado = input("Digite seu nome: ")
+boas_vindas(nome_digitado)
 
+
+def soma(num_a, num_b):
+    return num_a + num_b
+
+resultado = soma(7, 4)
+print(resultado)
+
+
+num_a = int(input("Digite o primeiro número: "))
+num_b = int(input("Digite o segundo número: "))
+print(soma(num_a, num_b))
+
+
+def pode_aprovar(idade):
+    if idade >= 18:
+        return True
+    else:
+        return False
+
+idade = int(input("Digite sua idade: "))
+print(pode_aprovar(idade))
